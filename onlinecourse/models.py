65f4f616572743f07267a0258befe618fb430431
VivekDeniz <133251644+VivekDeniz@users.noreply.github.com>
2023-06-15 13:22:52 -0400

    Update models.py

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -74,11 +74,9 @@ class Lesson(models.Model):
     order = models.IntegerField(default=0)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     content = models.TextField()
-
+    
 
 # Enrollment model
-# <HINT> Once a user enrolled a class, an enrollment entry should be created between the user and course
-# And we could use the enrollment to track information such as exam submissions
 class Enrollment(models.Model):
     AUDIT = 'audit'
     HONOR = 'honor'
@@ -89,60 +87,38 @@ class Enrollment(models.Model):
         (BETA, 'BETA')
     ]
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, default="")
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
     date_enrolled = models.DateField(default=now)
     mode = models.CharField(max_length=5, choices=COURSE_MODES, default=AUDIT)
     rating = models.FloatField(default=5.0)
 
 
-# <HINT> Create a Question Model with:
-    # Used to persist question content for a course
-    # Has a One-To-Many (or Many-To-Many if you want to reuse questions) relationship with course
-    # Has a grade point for each question
-    # Has question content
-    # Other fields and methods you would like to design
+# Question model
 class Question(models.Model):
-
-    # Foreign key to lesson
-    # question text
-    # question grade/mark
-
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
     lesson_id = models.ForeignKey(Lesson, on_delete=models.CASCADE)
-    question_txt = models.CharField(max_length = 200, default="question-text")
-    question_mark = models.IntegerField(default = 0)
-
+    gradePoint = models.IntegerField(default=2)
+    questionContent = models.CharField(max_length=35)
     def __str__(self):
-        return "Question: " + self.question_txt
-   
+        return "Question: " + self.questionContent
 
-    # <HINT> A sample model method to calculate if learner get the score of the question
+    # method to calculate if learner get the score of the question
     def is_get_score(self, selected_ids):
-        all_answers = self.choice_set.filter(is_correct=True).count()
-        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
-        if all_answers == selected_correct:
-            return True
-        else:
-            return False
+      all_answers = self.choice_set.filter(is_correct=True).count()
+      selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+      if all_answers == selected_correct:
+        return True
+      else:
+        return False
 
 
-#  <HINT> Create a Choice Model with:
-    # Used to persist choice content for a question
-    # One-To-Many (or Many-To-Many if you want to reuse choices) relationship with Question
-    # Choice content
-    # Indicate if this choice of the question is a correct one or not
-    # Other fields and methods you would like to design
+# Choice model
 class Choice(models.Model):
-    question_id = models.ForeignKey(Question, on_delete = models.CASCADE)
-    choice_text = models.CharField(max_length = 200, default="question-text")
-    is_correct = models.BooleanField(default = False)
-
-
-# <HINT> The submission model
-# One enrollment could have multiple submission
-# One submission could have multiple choices
-# One choice could belong to multiple submissions
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=35)
+    is_correct = models.BooleanField()
+    
+# Submission model
 class Submission(models.Model):
     enrollment_id = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-    chocies = models.ManyToManyField(Choice)
-
-#    Other fields and methods you would like to design
+    choices = models.ManyToManyField(Choice)
